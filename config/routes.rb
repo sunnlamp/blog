c8e6ede5f6c0b3bc336be8759554df389e07c17a
Rogelio Zamudio <yeyozaz@gmail.com>
2017-08-15 16:46:37 -0500

    added untracked changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :views
+  devise_for :users
   get 'pages/about'
 
   get 'sessions/new'
--- db/schema.rb
+++ db/schema.rb
@@ -20,14 +20,5 @@ ActiveRecord::Schema.define(version: 20170129042904) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-  end
 
 end
